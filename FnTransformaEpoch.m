6ee96092d5866e60c5b58ba6d6aa5f4492c84b05
Renato Lira <76776951+natolira@users.noreply.github.com>
2021-10-30 13:17:18 -0300

    Update FnTransformaEpoch.m

--- FnTransformaEpoch.m
+++ FnTransformaEpoch.m
@@ -1,10 +1,13 @@
 // FnTransformaEpoch
 // Função Criada por Renato Lira
-(Data as date) as text => 
-let
-    Fonte = #datetime(1970, 1, 1, 0, 0, 0),
-    Duracao = DateTime.From(Data) - Fonte,
-    TransformaSegundos = Duration.TotalSeconds(Duracao),
-    FormatoTexto = Text.From(TransformaSegundos)
-in
+// Última atualização em 30/10/2021
+// Transforma uma entrada no formato date ou datetime no formado de data/hora Epoch no formato texto
+// Utilizado no WebScrapping do Yahoo Finance
+(Data) as text =>
+  let
+    Fonte              = #datetime(1970, 1, 1, 0, 0, 0), 
+    Duracao            = DateTime.From(Data) - Fonte, 
+    TransformaSegundos = Duration.TotalSeconds(Duracao), 
+    FormatoTexto       = Text.From(TransformaSegundos)
+  in
     FormatoTexto
