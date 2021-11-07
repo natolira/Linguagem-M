067cc202dbb946ab18447c8f1d09f45c5cfd42cf
Renato Lira <76776951+natolira@users.noreply.github.com>
2021-11-07 12:09:43 -0300

    Update FnLimpaTexto.m

--- FnLimpaTexto.m
+++ FnLimpaTexto.m
@@ -1,5 +1,6 @@
 // FnLimpaTexto
 // Função Criada por Renato Lira
+// Buscar versões atualizadas em: https://github.com/natolira/Linguagem-M
 // Atualizada em 30/10/2021
 // Recebe um texto e remove caracteres de controle e espaços em excesso (no início, meio ou fim do texto)
 // Importante! Devido à recursividade, é necessário que o nome da consulta seja o mesmo: FnLimpaTexto 
