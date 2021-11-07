a6271944275d93d60dc531e30ab4dc52e299b44f
Renato Lira <76776951+natolira@users.noreply.github.com>
2021-11-07 12:44:42 -0300

    Update FnExtraiSGS.m

--- FnExtraiSGS.m
+++ FnExtraiSGS.m
@@ -5,6 +5,10 @@
 // Primeiro Argumento: número da série do SGS conforme https://www3.bcb.gov.br/sgspub/localizarseries/localizarSeries.do?method=prepararTelaLocalizarSeries
 // Segundo Argumento (opcional): data inicial a partir da qual os dados serão retornados
 // Terceiro Argumento (opcional):  data final a partir da qual os dados serão retornados
+//
+// !!! Método não oficial de obtenção de dados sujeito a API throttling 
+// Recomendado rodar essa rotina com uso de atualização incremental no Power BI Serviço
+// !!! Não rodar essa rotina múltiplas vezes ao dia. Rodar apenas 1x ao dia às 13:30, horário em que o dólar PTAX é divulgado.
 
 (serie, inicio, fim) as table =>
   let
