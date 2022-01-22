68d76b5e34e8c5c388ab2403b59723027e822519
Renato Lira <76776951+natolira@users.noreply.github.com>
2022-01-22 09:49:26 -0300

    Update FnMantemColunasComValores.m
    lidar com erros de fórmula do Excel no Power Query

--- FnMantemColunasComValores.m
+++ FnMantemColunasComValores.m
@@ -1,29 +1,45 @@
 // FnMantemColunasComValores
 // Função Criada por Renato Lira
 // Buscar versões atualizadas em: https://github.com/natolira/Linguagem-M
-// Última atualização em 07/11/2021
+// Última atualização em 21/01/2022
 // A partir de uma tabela (normalmente extraída do Excel) seleciona apenas colunas com valores
+// Parâmetro opcional, a partir de quantas linhas com valores manter a coluna
+// Parâmetro opcional, escrever qualquer coisa caso queira substituir erros do Excel por null
 
-(tbl as table) as table =>
+(tbl as table, optional corte as number, optional TrocaErroPorNull) as table =>
   let
-    Tabela = tbl,
-    ColunasDisponiveis = Table.ColumnNames(Tabela),
+    Tabela = tbl, 
+    Corte = corte ?? 0, 
+    erro = if TrocaErroPorNull <> null then "sim" else "nao", 
+    FnTrocaErroPorValor = (arg) =>
+      let
+        Fonte                 = arg, 
+        Colunas               = Table.ColumnNames(Fonte), 
+        Trocas                = List.Transform(Colunas, each {_, null}), 
+        #"Erros Substituídos" = Table.ReplaceErrorValues(Fonte, Trocas)
+      in
+        #"Erros Substituídos", 
+    TabelaTratada = if erro = "sim" then FnTrocaErroPorValor(Tabela) else Tabela, 
+    ColunasDisponiveis = Table.ColumnNames(TabelaTratada), 
     ColunasDisponiveisTabela = Table.FromList(
-      ColunasDisponiveis,
-      Splitter.SplitByNothing(),
-      null,
-      null,
+      ColunasDisponiveis, 
+      Splitter.SplitByNothing(), 
+      null, 
+      null, 
       ExtraValues.Error
-    ),
+    ), 
     MantemApenasNaoNulos = Table.AddColumn(
-      ColunasDisponiveisTabela,
-      "ValoresNaoNulos",
-      each List.Select(Table.Column(Tabela, [Column1]), each _ <> "" and _ <> null)
-    ),
+      ColunasDisponiveisTabela, 
+      "ValoresNaoNulos", 
+      each List.Select(
+        Table.Column(TabelaTratada, [Column1]), 
+        each try _ <> "" and _ <> null otherwise false
+      )
+    ), 
     ApenasColunasNaoNulas = Table.SelectRows(
-      MantemApenasNaoNulos,
-      each (List.Count([ValoresNaoNulos]) > 0)
-    ),
-    SelecionaColunasNaoNulas = Table.SelectColumns(Tabela, ApenasColunasNaoNulas[Column1])
+      MantemApenasNaoNulos, 
+      each (List.Count([ValoresNaoNulos]) > Corte)
+    ), 
+    SelecionaColunasNaoNulas = Table.SelectColumns(TabelaTratada, ApenasColunasNaoNulas[Column1])
   in
     SelecionaColunasNaoNulas
