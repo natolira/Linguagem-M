92901ac07f4dbb2d8166c41140be6dd28e2462fc
Renato Lira <76776951+natolira@users.noreply.github.com>
2023-03-31 15:07:12 -0300

    Create FnConverteTextoEmMinutos.m

---
+++ FnConverteTextoEmMinutos.m
@@ -0,0 +1,13 @@
+// FnConverteTextoEmMinutos
+// Aceita texto por extenso, ex: 2 dias 20 horas 1 minuto
+// e transforma na respectica quantidade em minutos
+(texto as text) as number =>
+let
+    Fonte = Text.Trim(texto),
+    TransformaEmLista = Text.Split(Fonte, " "),
+    removeS = List.ReplaceValue(TransformaEmLista, "s", "",  Replacer.ReplaceText),
+    Maiuscula = List.Transform( removeS, each Text.Upper(_)),
+    CriaRegistro = Record.FromList( List.Alternate(Maiuscula, 1, 1, 1), List.Alternate(Maiuscula, 1, 1)),
+    ResultadoEmMinutos = Number.From(try CriaRegistro[EGUNDO] otherwise 0)/60 + Number.From(try CriaRegistro[MINUTO] otherwise 0) + Number.From(try CriaRegistro[HORA] otherwise 0)*60 + Number.From(try CriaRegistro[DIA] otherwise 0)*60*24 + Number.From(try CriaRegistro[EMANA] otherwise 0)*60*24*7
+in
+    ResultadoEmMinutos
